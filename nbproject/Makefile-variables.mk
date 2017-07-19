4c4a069b9100ffdf42895541d277ffc4614e63ce
siplivit <siplivit@gmail.com>
2017-07-20 00:28:49 +0300

    Start

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=vendingmachinesparklingwater
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/vendingmachinesparklingwater
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=vendingmachinesparklingwater.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/vendingmachinesparklingwater.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=vendingmachinesparklingwater
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/vendingmachinesparklingwater
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=vendingmachinesparklingwater.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/vendingmachinesparklingwater.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
