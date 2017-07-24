045cdda5738484c5b93e469f855a0c5810421af8
Vitaliy Siplichuk <vsiplichuk@atncorp.com>
2017-07-24 18:39:30 +0300

    Added test function for loading and saving of settings

--- main.cpp
+++ main.cpp
@@ -27,59 +27,54 @@
 using json = nlohmann::json;
 using namespace std;
 
+namespace myjsonns {
+    void to_json(json& j, const settings_t& p) {
+        j = json{{"settings_general/currency", p.settings_general.currency},
+        {"settings_general/languageID", p.settings_general.languageID},
+        {"settings_general/products_amount", p.settings_general.products_amount},
+        {"cupDisp/cupTubesAmount", p.cupDisp.cupTubesAmount},
+        {"cupDisp/cupDropTime", p.cupDisp.cupDropTime},
+        {"cupDisp/cupDropSensorEnableFlg", p.cupDisp.cupDropSensorEnableFlg}};
+    }
+
+    void from_json(const json& j, settings_t& p) {
+        p.settings_general.currency = j.at("settings_general/currency").get<std::string>();
+        p.settings_general.languageID = j.at("settings_general/languageID").get<int>();
+        p.settings_general.products_amount = j.at("settings_general/products_amount").get<int>();
+        p.cupDisp.cupTubesAmount = j.at("cupDisp/cupTubesAmount").get<int>();
+        p.cupDisp.cupDropTime = j.at("cupDisp/cupDropTime").get<int>();
+        p.cupDisp.cupDropSensorEnableFlg = j.at("cupDisp/cupDropSensorEnableFlg").get<bool>();
+    }
+}
+
 /*
  * 
  */
 int main(int argc, char** argv) {
-    
+
+    // read a JSON file
+    std::ifstream i("test.json");
     json j;
+    i >> j;
 
-        // add a number that is stored as double (note the implicit conversion of j to an object)
-        j["pi"] = 3.141;
-
-        // add a Boolean that is stored as bool
-        j["happy"] = true;
-
-        // add a string that is stored as std::string
-        j["name"] = "Niels";
-
-        // add another null object by passing nullptr
-        j["nothing"] = nullptr;
-
-        // add an object inside the object
-        j["answer"]["everything"] = 42;
-
-        // add an array that is stored as std::vector (using an initializer list)
-        j["list"] = {1, 0, 2};
-
-        // add another object (using an initializer list of pairs)
-        j["object"] = { {"currency", "грн."}, {"value", 42.99} };
-     
-
-    /*try {
-        std::ifstream i("settings.json");
-        i >> j;
-    } catch (...) {
-        std::cout << "Exeption while settings.json reading" << std::endl;
-    }
-     */
-    if (j["object"].is_object()) {
-        string curr;
-        json obj = j["object"];
-        if (obj.find("currency") != obj.end()) {
-            curr = obj["currency"];
-            std::cout << "j[object][currency] = " << curr << std::endl;
-        }
-    }
-
-    // iterate the array
-    for (json::iterator it = j.begin(); it != j.end(); ++it) {
-        std::cout << *it << std::endl;
-    }
+    // conversion: json -> settings
+    myjsonns::settings_t settings = j;
+    
+    std::cout << settings.settings_general.currency << std::endl;
+    std::cout << settings.settings_general.languageID << std::endl;
+    std::cout << settings.settings_general.products_amount << std::endl;
+    std::cout << settings.cupDisp.cupTubesAmount<< std::endl;
+    std::cout << settings.cupDisp.cupDropTime<< std::endl;
+    std::cout << settings.cupDisp.cupDropSensorEnableFlg<< std::endl;
+    
+    std::ofstream o("test_out.json");
+    json j_out = settings;
+    o << j_out;
 
     //std::cout << std::setw(4) << j << std::endl;
     //std::cout << "j.size() " << j.size() << std::endl;
 
+    /*
     // find an entry
     if (j.find("currency") != j.end()) {
         std::cout << "there is an entry with key currency" << std::endl;
@@ -87,15 +82,11 @@ int main(int argc, char** argv) {
 
     std::ofstream o("settings.json");
     o << std::setw(4) << j << std::endl;
-    
-    
-    
-    
-    
-    
+     */
+
     Machine* machine = new Machine();
     machine->connect();
-    
+
     delete machine;
 
     return 0;
